36e357e440f6dfddbdbc374948077c57bb24f23a
Eric Ekas <eric.ekas@gmail.com>
2022-05-19 11:36:00 -0400

    commit tree f728455192dfeb277b8bf10081ddcad29edcce18

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+    ;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+function writeCards(names, event) {
+    const cards = []
+    for(let name of names) {
+        cards.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(count) {
+    while(count >= 0) {
+        console.log(count)
+        count -= 1
+    }
+}
